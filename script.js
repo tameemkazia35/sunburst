f1f268f1b6996129ac389fd0938e1b873590c030
tameemkazia35 <tameemkazia35@gmail.com>
2018-12-05 11:54:30 +0400

    TPD employess

--- script.js
+++ script.js
@@ -1,225 +1,5 @@
 var data;
-/*var data = [{
-    'id': '1',
-    'parent': '',
-    'name': 'الخطة الإستراتيجية 2018-2020'
-}, {
-    'id': '1.1',
-    'parent': '1',
-    'name': 'خدمات تقاعدية تثري تجربة المتعاملين',
-}, 
 
- 
-
-{
-    'id': '2.1',
-    'parent': '1.1',
-    'name': 'الرضا العام عن خدمات الصندوق',
-    'value': 55
-    
-},
-{
-    'id': '2.2',
-    'parent': '1.1',
-    'name': 'التميز في رحلة المتعامل',
-    'value': 45
-},
-
-{
-    'id': '2.2.1',
-    'parent': '2.2',
-    'name': '% فعالية قنوات الخدمة',
-    'value': 28.69
-},
-{
-    'id': '2.2.2',
-    'parent': '2.2',
-    'name': '% جودة الخدمات (الوقت، الدقة)',
-    'value': 16.31
-},
-
-{
-    'id': '2.1.1',
-    'parent': '2.1',
-    'name': 'الرضا العام عن خدمات الصندوق',
-    'value': 19.25
-},
-
-{
-    'id': '2.1.2',
-    'parent': '2.1',
-    'name': 'نتيجة مركز الاتصال الحكومي',
-    'value': 16.5
-}, {
-    'id': '2.1.3',
-    'parent': '2.1',
-    'name': 'نتيجة الاستبيان الحكومي',
-    'value': 19.25
-},
-
-{
-    'id': '2.1.1.1',
-    'parent': '2.1.1',
-    'name': '% الانجاز من حزمة إدارة تقنية المعلومات الخاص بالمتعامل السري',
-    'value': 4.43
-},
-
-{
-    'id': '2.1.1.2',
-    'parent': '2.1.1',
-    'name': '% الانجاز من حزمة إدارة الاتصال المؤسسي الخاص بالمتعامل السري',
-    'value': 4.24
-}, {
-    'id': '2.1.1.3',
-    'parent': '2.1.1',
-    'name': 'الانجاز من حزمة دائرة المعاشات الخاص بالمتعامل السري',
-    'value': 8.86
-}, {
-    'id': '2.1.1.4',
-    'parent': '2.1.1',
-    'name': 'الانجاز من حزمة وحدة الخدمات العامة الخاص بالمتعامل السري',
-    'value': 1.73
-},
-{
-    'id': '2.1.2.1',
-    'parent': '2.1.2',
-    'name': '% حل الحالات من أول مكالمة',
-    'value': 2.48
-},
-{
-    'id': '2.1.2.2',
-    'parent': '2.1.2',
-    'name': '% دقة الرد',
-    'value': 3.30
-},
-{
-    'id': '2.1.2.3',
-    'parent': '2.1.2',
-    'name': '% امتثال الجهة لاتفاق مستوى الخدمة',
-    'value': 2.48
-},
-{
-    'id': '2.1.2.4',
-    'parent': '2.1.2',
-    'name': '% مؤشر القبول',
-    'value': 4.13
-},
-{
-    'id': '2.1.2.5',
-    'parent': '2.1.2',
-    'name': '% متوسط فترة الرد',
-    'value': 4.13
-},
- {
-    'id': '2.1.3.1',
-    'parent': '2.1.3',
-    'name': '% الانجاز من معيار توفر المعلومات',
-    'value': 2.89
-},
- {
-    'id': '2.1.3.2',
-    'parent': '2.1.3',
-    'name': '% الانجاز من نتيجة معيار الوصول إلى الجهة',
-    'value': 2.89
-},
- {
-    'id': '2.1.3.3',
-    'parent': '2.1.3',
-    'name': '% الانجاز من ر معيار قنوات تقديم الخدمة',
-    'value': 2.89
-},
- {
-    'id': '2.1.3.4',
-    'parent': '2.1.3',
-    'name': '% الانجاز من معيار الفترة اللازمة لتقديم الخدمة',
-    'value': 2.89
-},
- {
-    'id': '2.1.3.5',
-    'parent': '2.1.3',
-    'name': '% الانجاز من معيار الموظفون',
-    'value': 3.85
-},
- {
-    'id': '2.1.3.6',
-    'parent': '2.1.3',
-    'name': '% الانجاز من معيار إدارة الشكاوى و المقترحات',
-    'value': 3.85
-},
-{
-    'id': '2.2.1.1',
-    'parent': '2.2.1',
-    'name': '% فعالية الموقع الالكتروني',
-    'value': 6.75
-},
-{
-    'id': '2.2.1.2',
-    'parent': '2.2.1',
-    'name': '% فعالية الموقع الالكتروني',
-    'value': 3.38
-},
-{
-    'id': '2.2.1.3',
-    'parent': '2.2.1',
-    'name': '% فعالية الموقع الالكتروني',
-    'value': 1.69
-},
-{
-    'id': '2.2.1.4',
-    'parent': '2.2.1',
-    'name': '% فعالية التطبيقات الذكية',
-    'value': 5.06
-},
-{
-    'id': '2.2.1.5',
-    'parent': '2.2.1',
-    'name': '% فعالية التطبيقات الذكية',
-    'value': 3.38
-},
-{
-    'id': '2.2.1.6',
-    'parent': '2.2.1',
-    'name': '% فعالية التطبيقات الذكية',
-    'value': 3.38
-},
-{
-    'id': '2.2.1.7',
-    'parent': '2.2.1',
-    'name': '% فعالية الواجهة الأمامية خدمة العملاء',
-    'value': 3.38
-},
-{
-    'id': '2.2.1.8',
-    'parent': '2.2.1',
-    'name': '% فعالية الواجهة الأمامية خدمة العملاء',
-    'value': 1.69
-},
-{
-    'id': '2.2.2.1',
-    'parent': '2.2.2',
-    'name': '% الالتزام الزمني بتقديم الخدمات',
-    'value': 3.38
-},
-{
-    'id': '2.2.2.2',
-    'parent': '2.2.2',
-    'name': '% الالتزام بدقة الخدمات',
-    'value': 1.69
-},
-{
-    'id': '2.2.2.3',
-    'parent': '2.2.2',
-    'name': '% فعالية مركز الاتصال',
-    'value': 5.63
-},
-{
-    'id': '2.2.2.4',
-    'parent': '2.2.2',
-    'name': '% فعالية مركز الاتصال',
-    'value': 5.63
-},
-];
-*/
 // Splice in transparent for the center circle
 Highcharts.getOptions().colors.splice(0, 0, '#f9f9f9');
  //Highcharts.getOptions().colors.splice(1, 0, '#decbe4');
@@ -229,9 +9,25 @@ Highcharts.getOptions().colors.splice(2, 0, '#ccebc5');
     colors: ['#decbe4', '#decbe4', '#decbe4', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#decbe4']
 });  */
 
+var TPD_emp = [{dataId:'department-TPD-Abdulaziz_alrashdi', name:'Abdulaziz Alrashdi', filename:'department-TPD-Abdulaziz_alrashdi.json'},
+{dataId:'department-TPD-Aida_almansouri', name:'Aida Almansouri', filename:'department-TPD-Aida_almansouri.json'},
+{dataId:'department-TPD-Ali_Almushgri', name:'Ali Almushgri', filename:'department-TPD-Ali_Almushgri.json'},
+{dataId:'department-TPD-Essa_alzaabi', name:'Essa Alzaabi', filename:'department-TPD-Essa_alzaabi.json'},
+{dataId:'department-TPD-Mariam_Almazroui', name:'Mariam Almazroui', filename:'department-TPD-Mariam_Almazroui.json'},
+{dataId:'department-TPD-Mubarak_Alhumairi', name:'Mubarak Alhumairi', filename:'department-TPD-Mubarak_Alhumairi.json'},
+{dataId:'department-TPD-Noura_AlKhalili', name:'Noura AlKhalili', filename:'department-TPD-Noura_AlKhalili.json'},
+{dataId:'department-TPD-Reem_Alzaabi', name:'Reem Alzaabi', filename:'department-TPD-Reem_Alzaabi.json'},
+{dataId:'department-TPD-Safiya_Alhamadi', name:'Safiya Alhamadi', filename:'department-TPD-Safiya_Alhamadi.json'},
+{dataId:'department-TPD-Saif_Aldahri', name:'Saif Aldahri', filename:'department-TPD-Saif_Aldahri.json'},
+{dataId:'department-TPD-Saleh_Alburaiki', name:'Saleh Alburaiki', filename:'department-TPD-Saleh_Alburaiki.json'},
+{dataId:'department-TPD-Suhail_almazroui', name:'Suhail Almazroui', filename:'department-TPD-Suhail_almazroui.json'},
+{dataId:'department-TPD-Shama_Almansouri', name:'Shama Almansouri', filename:'department-TPD-Shama_Almansouri.json'}
+];
+
  var defaultJson = 'json/orgnization.json';
  jQuery.getJSON(defaultJson, function(_data) {
                //data = _data;
+			   
                renderChart(_data);
  });
 
@@ -355,18 +151,32 @@ $("#btnPng").on('click', function (event) {
         type: "image/png"
     });
 });
+
 $("#btnPdf").on('click', function (event) {
     chart.exportChart({
         type: "application/pdf"
     });
 });
+
 $("#btnSvg").on('click', function (event) {
     chart.exportChart({
         type: "application/svg"
     });
 });
 
+$(document).on('click',".emp-menu li a", function(){
+	 $(this).parents(".dropdown").find('.append-text').html($(this).find('span').text());
+    $(this).parents(".dropdown").find('.append-text').val($(this).data('value'));
+	debugger;
+	var _jsonURL =  'json/emp/'+$(this).attr('data-value')+'.json';
+	jQuery.getJSON(_jsonURL, function(_data) {
+            //data = _data;
+            renderChart(_data);
+});
+});
+
 $(".department-menu li a").click(function(){
+	$('.department-emp').css('display','none');
     $(this).parents(".dropdown").find('.append-text').html($(this).find('span').text());
     $(this).parents(".dropdown").find('.append-text').val($(this).data('value'));
     if($(this).find('span').text() == "HR DEPARTMENT"){
@@ -427,6 +237,20 @@ $(".department-menu li a").click(function(){
     }
 	if($(this).find('span').text() == "PC GS DEPARTMENT"){
         jQuery.getJSON('json/department-PCGS.json', function(_data) {
+            //data = _data;
+            renderChart(_data);
+});
+    }
+	if($(this).find('span').text() == "TPD DEPARTMENT"){
+        jQuery.getJSON('json/department-TPD.json', function(_data) {
+			if(TPD_emp.length > 0){
+				$('.department-emp').css('display','inline-flex');
+				var _html = '';
+				for(i = 0; i < TPD_emp.length; i++){
+					_html = _html + "<li><a class='dropdown-item' href='#' data-value='"+TPD_emp[i].dataId+"'><span>"+TPD_emp[i].name+"</span><br><small>TPD Department Board</small></a></li>";
+				}
+				$('.emp-menu').append(_html);
+			}
             //data = _data;
             renderChart(_data);
 });
